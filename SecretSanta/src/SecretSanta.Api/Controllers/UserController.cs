a91a17251b74477da30501f7aedb7d27e8276cec
Calum Mackenzie <cmackenzie@eagles.ewu.edu>
2021-04-25 21:25:18 -0700

    Added all basic crud functions and rest client.

--- SecretSanta/src/SecretSanta.Api/Controllers/UserController.cs
+++ SecretSanta/src/SecretSanta.Api/Controllers/UserController.cs
@@ -8,19 +8,38 @@ namespace SecretSanta.Api.Controllers
     [ApiController]//cal: just trust that this is needed.
     public class UserController: ControllerBase//cal: use ControllerBase when the controller doesent need to return a view.
     {
-        
+        //route: api/user/
         [HttpGet]//cal: This is equivalent to a Read
         public IEnumerable<string> Get()
         {
             return TestData.Users;
         }
 
-        
-        //[Route("api/[controller]/{index}")]
+        //route: api/user/[index]
         [HttpGet("{index}")]
         public string Get(int index)
         {
             return TestData.Users[index];
+        }
+
+        //route: api/user/[index]
+        [HttpDelete("{index}")]//cal: equivalent to delete
+        public void Delete(int index)
+        {
+            TestData.Users.RemoveAt(index);
+        }
+
+        //route:api/user
+        [HttpPost]//cal: equivalent to create
+        public void Post([FromBody] string userName)
+        {
+            TestData.Users.Add(userName);
+        }
+
+        [HttpPut("{index}")]//cal: equivalent to update
+        public void Put(int index, [FromBody]string userName)
+        {
+            TestData.Users[index] = userName;
         }
         
     }
