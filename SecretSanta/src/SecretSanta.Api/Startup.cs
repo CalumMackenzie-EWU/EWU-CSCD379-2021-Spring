10f92a9d23a86b97bc27ce579452a776ea1c65ab
Calum Mackenzie <cmackenzie@eagles.ewu.edu>
2021-05-02 18:40:12 -0700

    Added NSwag specifics and event manager.

--- SecretSanta/src/SecretSanta.Api/Startup.cs
+++ SecretSanta/src/SecretSanta.Api/Startup.cs
@@ -14,6 +14,8 @@ namespace SecretSanta.Api
         {
             services.AddScoped<IUserRepository, UserRepository>();
             services.AddControllers();
+            
+            services.AddSwaggerDocument();
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
@@ -24,6 +26,7 @@ namespace SecretSanta.Api
                 app.UseDeveloperExceptionPage();
             }
 
+            //cal: order inside Configure does matter.
             app.UseOpenApi();
             app.UseSwaggerUi3();
 
