859011fda5d283e7fcc1e47ccc910c36e1bbdda5
Calum Mackenzie <cmackenzie@eagles.ewu.edu>
2021-05-17 22:03:28 -0700

    Adding changes for Alpline Js. Currently not loading user data.

--- SecretSanta/src/SecretSanta.Api/Startup.cs
+++ SecretSanta/src/SecretSanta.Api/Startup.cs
@@ -15,6 +15,19 @@ namespace SecretSanta.Api
             services.AddScoped<IUserRepository, UserRepository>();
             services.AddControllers();
             services.AddSwaggerDocument();
+
+            //cal:This stuff below is added for assingment 7
+            //cal: this stuff is not secure. Only used for our class purposes.
+            //cal: Cors deals with cross origins, e.g our Web calling the Api
+            services.AddCors(options =>
+            {
+                options.AddDefaultPolicy(builder =>
+                {
+                    builder.AllowAnyOrigin()
+                           .AllowAnyMethod()
+                           .AllowAnyHeader();
+                });
+            });
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
@@ -29,6 +42,8 @@ namespace SecretSanta.Api
             app.UseSwaggerUi3();
 
             app.UseRouting();
+
+            app.UseCors();//cal: this is from Asmt7. Must come after routing, and before endpoints.
 
             app.UseEndpoints(endpoints =>
             {
--- SecretSanta/src/SecretSanta.Web/scripts/main.ts
+++ SecretSanta/src/SecretSanta.Web/scripts/main.ts
@@ -1,14 +1,18 @@
 import '../styles/site.css';
 
 import 'alpinejs';
+import axious from 'axios';
 
 import { library, dom } from "@fortawesome/fontawesome-svg-core";
 import { fas } from '@fortawesome/free-solid-svg-icons';
 import { far } from '@fortawesome/free-regular-svg-icons';
 import { fab } from '@fortawesome/free-brands-svg-icons';
+import axios from 'axios';
 
 library.add(fas, far, fab);
 dom.watch();
+
+interface User{}
 
 export function setupNav() {
     return {
@@ -20,6 +24,24 @@ export function setupNav() {
                 } else {
                     headerNav.classList.add('hidden');
                 }
+            }
+        }
+    }
+}
+
+//cal: This stuff got changed around when converting to Alpine.js. Remeber that inorder to use these things we put them inside a x-data attribute in the html.
+//cal: we also imported axios into out package.json.
+//cal: javascript is camel case while c# is pascal case.
+//cal: we change the code inside the views because we are no longer using server side code.
+export function setupUsers() {
+    return {
+        users: [] as User[],
+        async mounted(){//cal: this is where we make a call to the Api. Also be aware that everything in js is asynchronous. Also the name mounted matches what we have in html. But could be whatever we wanted, e.g loadUsers().
+            try{
+                const response = await axios.get("https://localhost:5101/api/users");
+                this.users = response.data;
+            }catch(error){
+                console.log(error);
             }
         }
     }
