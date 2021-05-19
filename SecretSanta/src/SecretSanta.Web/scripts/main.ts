88b457fa25bfea4d02919d65e759f4139959b102
Calum Mackenzie <cmackenzie@eagles.ewu.edu>
2021-05-18 23:07:34 -0700

    Lots of changes around the place to get things working.
    Changed some html and added ApiHost stuff to the main.ts

--- SecretSanta/src/SecretSanta.Web/scripts/main.ts
+++ SecretSanta/src/SecretSanta.Web/scripts/main.ts
@@ -12,6 +12,8 @@ import axios from 'axios';
 library.add(fas, far, fab);
 dom.watch();
 
+declare var apiHost: string;//cal: This does not define the var. It just lets typescript know that this var will be injected in. In this case we get it from our _Layout_Template.cshtml
+
 interface User{//cal: This just helps with dot notation. Doesent necessarily need every field.
     firstName: string,
     lastName: string,
@@ -49,7 +51,8 @@ export function setupUsers() {
         {
             if(confirm(`Are you sure you wish to delete ${currentUser.firstName} ${currentUser.lastName}?`))
             {
-                await axios.delete(`https://localhost:5101/api/users/${currentUser.id}`);//cal: keep in mind that this isnt a single quote. Its from the tilde key.
+                //await axios.delete(`https://localhost:5101/api/users/${currentUser.id}`);//cal: keep in mind that this isnt a single quote. Its from the tilde key.
+                await axios.delete(`${apiHost}/api/users/${currentUser.id}`);//cal:This stuff replaced the above stuff since we couldnt guarantee to be on 5101. So were using our ApiHost.
                 await this.loadUsers();
             }
         },
@@ -57,7 +60,8 @@ export function setupUsers() {
         async loadUsers()//cal: the reason we moved this into loadUsers() was so it can be called like a page refresh in other functions
         {
             try{
-                const response = await axios.get("https://localhost:5101/api/users");
+                //const response = await axios.get("https://localhost:5101/api/users");
+                const response = await axios.get(`${apiHost}/api/users`);
                 this.users = response.data;
             }catch(error){
                 console.log(error);
@@ -73,7 +77,8 @@ export function createOrUpdateUser(){
         {
             try{
                 //cal: this part isnt for our assingment. But when working with a date you may need to convert the object by wrapping it as a this.user.date = new Date(this.user.date);
-                await axios.post("https://localhost:5101/api/users", this.user);
+                //await axios.post("https://localhost:5101/api/users", this.user);
+                await axios.post(`${apiHost}/api/users`, this.user);
                 window.location.href="/users";//cal: navigates back to users page.
             }catch(error){
                 console.log(error);
