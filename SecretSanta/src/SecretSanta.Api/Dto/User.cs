223f876249b1b7c96f8ea780dc703fe2580192ff
Calum Mackenzie <cmackenzie@eagles.ewu.edu>
2021-06-10 19:29:22 -0700

    Changes to Dto.User

--- SecretSanta/src/SecretSanta.Api/Dto/User.cs
+++ SecretSanta/src/SecretSanta.Api/Dto/User.cs
@@ -1,19 +1,29 @@
-﻿namespace SecretSanta.Api.Dto
+﻿using System.Collections.Generic;
+
+namespace SecretSanta.Api.Dto
 {
     public class User
     {
         public int Id { get; set; }
         public string? FirstName { get; set; }
         public string? LastName { get; set; }
+        //cal: added during display.
+        public List<User> AssignmentList{get;set;}
+        //public List<Gift> GiiftList{get;set;}
+        //
 
-        public static User? ToDto(Data.User? user)
+        //public static User? ToDto(Data.User? user)
+        public static User? ToDto(Data.User? user, List<User>? assignmentList = null)
         {
             if (user is null) return null;
             return new User
             {
                 FirstName = user.FirstName,
                 Id = user.Id,
-                LastName = user.LastName
+                LastName = user.LastName,
+
+                //
+                AssignmentList = assignmentList ?? new()
             };
         }
 
